518656845be748d9d888882cfa2f52f10a02c6db
Sandra Gadomska <gadomska.sandra@gmail.com>
2020-04-14 13:38:24 +0200

    initial commit

---
+++ src/main/java/com/gadomska/springbootopenid/SpringBootOpenidApplication.java
@@ -0,0 +1,13 @@
+package com.gadomska.springbootopenid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gadomska/springbootopenid/SpringBootOpenidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadomska.springbootopenid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
